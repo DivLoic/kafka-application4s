1b65d38f2cce480c0dc887f55e4c47b39e42d0bb
DivLoic <loicm.divad@gmail.com>
2020-07-09 08:58:26 +0200

    reformat the dependencies lists

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,18 +2,16 @@ import sbt._
 
 object Dependencies {
 
-  lazy val rootDependencies: List[ModuleID] =
-    "com.github.pureconfig" %% "pureconfig" % "0.13.0" ::
-      "org.scalatest" %% "scalatest" % "3.1.1" % Test :: Nil
+  lazy val rootDependencies: List[ModuleID] = "org.typelevel" %% "cats-core" % "2.1.1" ::
+                                              "ch.qos.logback" % "logback-classic" % "1.2.3" ::
+                                              "com.github.pureconfig" %% "pureconfig" % "0.13.0" ::
+                                              "org.scalatest" %% "scalatest" % "3.1.1" % Test :: Nil
 
-  lazy val kafkaClientsDeps: List[ModuleID] =
-    "org.apache.kafka" % "kafka-clients" % "2.5.0" :: Nil
+  lazy val kafkaClientsDeps: List[ModuleID] = "org.apache.kafka" % "kafka-clients" % "2.5.0" :: Nil
 
-  lazy val kafkaStreamsDeps: List[ModuleID] =
-    "org.apache.kafka" %% "kafka-streams-scala" % "2.5.0" ::
-      "org.apache.kafka" % "kafka-streams-test-utils" % "2.5.0" % Test :: Nil
+  lazy val kafkaStreamsDeps: List[ModuleID] = "org.apache.kafka" %% "kafka-streams-scala" % "2.5.0" ::
+                                              "org.apache.kafka" % "kafka-streams-test-utils" % "2.5.0" % Test :: Nil
 
-  lazy val kafkaContribDeps: List[ModuleID] =
-    "com.typesafe.akka" %% "akka-stream-kafka" % "2.0.3" ::
-      "com.github.fd4s" %% "fs2-kafka" % "1.0.0" :: Nil
+  lazy val kafkaContribDeps: List[ModuleID] = "com.typesafe.akka" %% "akka-stream-kafka" % "2.0.3" ::
+                                              "com.github.fd4s" %% "fs2-kafka" % "1.0.0" :: Nil
 }
