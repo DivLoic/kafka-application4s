cf98fb09fc1fee5218c5134d2ee6d2ff30e126fa
DivLoic <loicm.divad@gmail.com>
2020-07-15 06:47:31 +0200

    Add the serialization helpers

---
+++ src/main/scala/fr/ps/eng/kafka/app4s/common/HelperSerdes.scala
@@ -0,0 +1,39 @@
+package fr.ps.eng.kafka.app4s.common
+
+import com.sksamuel.avro4s.RecordFormat
+import io.confluent.kafka.streams.serdes.avro.{GenericAvroDeserializer, GenericAvroSerializer}
+import org.apache.kafka.common.serialization.{Deserializer, Serializer}
+/**
+ * Created by loicmdivad.
+ */
+trait HelperSerdes {
+
+    implicit lazy val keyFormat: RecordFormat[Key] = RecordFormat[Key]
+    implicit lazy val tvShowFormat: RecordFormat[TvShow] = RecordFormat[TvShow]
+    implicit lazy val ratingFormat: RecordFormat[Rating] = RecordFormat[Rating]
+
+    def reflectionAvroSerializer4S[T: RecordFormat]: Serializer[T] = new Serializer[T] {
+      val inner = new GenericAvroSerializer()
+
+      override def configure(configs: java.util.Map[String, _], isKey: Boolean): Unit = inner.configure(configs, isKey)
+
+      override def serialize(topic: String, maybeData: T): Array[Byte] = Option(maybeData)
+        .map(data => inner.serialize(topic, implicitly[RecordFormat[T]].to(data)))
+        .getOrElse(Array.emptyByteArray)
+
+      override def close(): Unit = inner.close()
+    }
+
+    def reflectionAvroDeserializer4S[T: RecordFormat]: Deserializer[T] = new Deserializer[T] {
+      val inner = new GenericAvroDeserializer()
+
+      override def configure(configs: java.util.Map[String, _], isKey: Boolean): Unit = inner.configure(configs, isKey)
+
+      override def deserialize(topic: String, maybeData: Array[Byte]): T = Option(maybeData)
+        .filter(_.nonEmpty)
+        .map(data => implicitly[RecordFormat[T]].from(inner.deserialize(topic, data)))
+        .getOrElse(null.asInstanceOf[T])
+
+      override def close(): Unit = inner.close()
+    }
+}
