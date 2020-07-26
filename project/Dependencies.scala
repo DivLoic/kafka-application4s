a873b4374ca11ac58671661ed2274d93710ae624
DivLoic <loicm.divad@gmail.com>
2020-07-26 20:15:43 +0200

    Update Kafka Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,10 +11,10 @@ object Dependencies {
                                               "io.confluent" % "kafka-streams-avro-serde" % "5.5.1" ::
                                               "org.scalatest" %% "scalatest" % "3.1.1" % Test :: Nil
 
-  lazy val kafkaClientsDeps: List[ModuleID] = "org.apache.kafka" % "kafka-clients" % "2.5.0" :: Nil
+  lazy val kafkaClientsDeps: List[ModuleID] = "org.apache.kafka" % "kafka-clients" % "2.6.0" :: Nil
 
-  lazy val kafkaStreamsDeps: List[ModuleID] = "org.apache.kafka" %% "kafka-streams-scala" % "2.5.0" ::
-                                              "org.apache.kafka" % "kafka-streams-test-utils" % "2.5.0" % Test :: Nil
+  lazy val kafkaStreamsDeps: List[ModuleID] = "org.apache.kafka" %% "kafka-streams-scala" % "2.6.0" ::
+                                              "org.apache.kafka" % "kafka-streams-test-utils" % "2.6.0" % Test :: Nil
 
   lazy val kafkaContribDeps: List[ModuleID] = "com.typesafe.akka" %% "akka-stream-kafka" % "2.0.3" ::
                                               "com.github.fd4s" %% "fs2-kafka" % "1.0.0" :: Nil
