642262268d97febbfc93c14ba344849c05968ee4
DivLoic <loicm.divad@gmail.com>
2020-07-27 20:55:57 +0200

    Reformat the project dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,14 +8,8 @@ object Dependencies {
                                               "com.sksamuel.avro4s" %% "avro4s-core" % "3.1.1" ::
                                               "com.nrinaudo" %% "kantan.csv" % "0.6.1" ::
                                               "com.nrinaudo" %% "kantan.csv-enumeratum" % "0.6.1" ::
-                                              "io.confluent" % "kafka-streams-avro-serde" % "5.5.1" ::
                                               "org.scalatest" %% "scalatest" % "3.1.1" % Test :: Nil
 
-  lazy val kafkaClientsDeps: List[ModuleID] = "org.apache.kafka" % "kafka-clients" % "2.6.0" :: Nil
-
-  lazy val kafkaStreamsDeps: List[ModuleID] = "org.apache.kafka" %% "kafka-streams-scala" % "2.6.0" ::
-                                              "org.apache.kafka" % "kafka-streams-test-utils" % "2.6.0" % Test :: Nil
-
-  lazy val kafkaContribDeps: List[ModuleID] = "com.typesafe.akka" %% "akka-stream-kafka" % "2.0.3" ::
-                                              "com.github.fd4s" %% "fs2-kafka" % "1.0.0" :: Nil
+  lazy val kafkaClientsDeps: List[ModuleID] = "org.apache.kafka" % "kafka-clients" % "2.6.0" ::
+                                              "io.confluent" % "kafka-streams-avro-serde" % "5.5.1" ::Nil
 }
