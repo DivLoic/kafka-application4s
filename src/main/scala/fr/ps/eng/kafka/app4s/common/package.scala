98d1fd740480c539e4b865aa5fe9800f66652d88
DivLoic <loicm.divad@gmail.com>
2022-01-02 12:42:46 +0100

    remove deprecated imports from the data generator

--- src/main/scala/fr/ps/eng/kafka/app4s/common/package.scala
+++ src/main/scala/fr/ps/eng/kafka/app4s/common/package.scala
@@ -1,16 +1,17 @@
 package fr.ps.eng.kafka.app4s
 
+import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
-
-import sun.misc.BASE64Encoder
+import java.util.Base64
 
 /**
  * Created by loicmdivad.
  */
 package object common {
 
-  val Base64Encoder: BASE64Encoder = new sun.misc.BASE64Encoder()
+  val Base64Encoder: Base64.Encoder = java.util.Base64.getEncoder
   val SHA1Digest: MessageDigest = java.security.MessageDigest.getInstance("SHA-1")
 
-  def sha1(name: String): String = Base64Encoder.encode(SHA1Digest.digest(name.getBytes))
+  def sha1(name: String): String =
+    new String(Base64Encoder.encode(SHA1Digest.digest(name.getBytes)), StandardCharsets.UTF_8)
 }
